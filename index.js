60680acb3c9641978698b91206cdb6094046ba2c
Shravan Nander Pandala <97742905+Unknown-Geek@users.noreply.github.com>
2024-08-29 01:08:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,13 +18,41 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date);
+  if (isInvalidDate(date)){
+    date = new Date(+req.params.date);
+  }
+
+  if (isInvalidDate(date)){
+    res.json({error : "Invalid Date"});
+    return;
+  }
+
+  res.json({"unix":date.getTime(),"utc": date.toUTCString()})
 });
 
 
+app.get("/api", function (req, res) {
+  let date = new Date()
+  res.json({"unix":date.getTime(),"utc": date.toUTCString()})
+});
+
+app.get("/api/:unix", function (req, res) {
+  let unix = new Date(req.params.unix);
+  if (isInvalidDate(date)){
+    date = new Date(+req.params.date);
+  }
+
+  if (isInvalidDate(date)){
+    res.json({error : "Invalid Date"});
+    return;
+  }
+
+  res.json({"unix": unix,"utc": date.toUTCString()})
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
